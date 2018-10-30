34a6c448601db899152f4c564df3dd96d5116787
wjackter <wjackter@gmail.com>
2018-10-30 11:49:07 -0400

    Opening dev tools by default as detached Added proper id to each piece Added drag event to each piece Allowing console in eslint rules

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ function createWindow() {
   // and load the index.html of the app.
   mainWindow.loadFile('index.html');
 
+  mainWindow.webContents.openDevTools({ mode: 'detach' });
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
---
+++ scripts/game.js
@@ -0,0 +1,19 @@
+/* global document window console */
+
+function setupPieceDragging(piece) {
+  console.log(piece);
+  piece.addEventListener('dragstart', (ev) => {
+    console.log(`start dragging: ${ev.target.id}`);
+  });
+}
+
+function setupGame() {
+  console.log('setupGame');
+  const gameBoard = document.querySelector('#game-board');
+  const pieces = gameBoard.querySelectorAll('span.piece');
+  for (let x = 0; x < pieces.length; x += 1) {
+    setupPieceDragging(pieces[x]);
+  }
+}
+
+window.addEventListener('load', setupGame);
